096eaf1fcbd3ae34b4bda0af7ffdf5e296070860
Chris West <chris@minrivertea.com>
2013-12-07 15:48:58 +0800

    updated static error pages

--- fuzzy/settings.py
+++ fuzzy/settings.py
@@ -26,7 +26,7 @@ SITE_URL =                          'http://www.fuzzymoose.co.uk'
 SITE_DOMAIN =                       'www.fuzzymoose.co.uk'
 SITE_EMAIL =                        'Angelique <info@fuzzymoose.co.uk>'
 CONTACT_EMAIL =                     'info@fuzzymoose.co.uk'
-SERVER_EMAIL =                      CONTACT_EMAIL
+SERVER_EMAIL =                      'chris@minrivertea.com'
 
 DATABASES = {
     'default': {
